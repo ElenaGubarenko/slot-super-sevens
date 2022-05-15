595faf34ed3bed993f98c75046fcf325d767cdc4
Elena Gubarenko <lenagritsishina@gmail.com>
2022-05-15 15:27:17 +0300

    add ch

--- src/Drum/Drum.js
+++ src/Drum/Drum.js
@@ -1,15 +1,14 @@
 import styles from "./Drum.module.css"
+import options from "../Utils/options"
 import { v4 as uuidv4 } from "uuid"
 
 function Drum(data) {
   return (
-    <>
-      <div className={styles.Drum}>
-        {data.data.map((element) => {
-          return <div key={uuidv4()}>{element}</div>
-        })}
-      </div>
-    </>
+    <div className={styles.Drum}>
+      {data.data.map((element) => {
+        return <div className={styles.DrumElement} key={uuidv4()}>{options.coloredPids[element]}</div>
+      })}
+    </div>
   )
 }
 
--- src/SuperSeven/SuperSeven.js
+++ src/SuperSeven/SuperSeven.js
@@ -6,7 +6,7 @@ import { v4 as uuidv4 } from "uuid"
 
 function SuperSeven() {
   const [lines, setLines] = useState(5)
-  const [credits, setCredits] = useState(1000)
+  const [credits, setCredits] = useState(10000)
   const [gameBet, setGameBet] = useState(100)
   const [totalBet, setTotalBet] = useState(0)
   const [gain, setGain] = useState(0)
@@ -14,6 +14,7 @@ function SuperSeven() {
   const [drumsData, setDrumsData] = useState({})
   const [gainData, setGainData] = useState(0)
   const [changedPids, setChangedPids] = useState({})
+  const [gameOver, setGameOver] = useState(false)
 
   useEffect(() => {
     const changedOptions = options.pids
@@ -33,7 +34,6 @@ function SuperSeven() {
       const randomIndex = Math.floor(Math.random() * data.length)
       const randomNumber = data[randomIndex]
       const randomFruit = changedPids[randomNumber]
-      // console.log(changedPids[randomNumber])
       result.push(randomFruit)
     }
 
@@ -61,11 +61,14 @@ function SuperSeven() {
   }
 
   const countGain = () => {
+    setGain(0)
     setGainData(0)
     let numberOfIndex = 0
     const gainElements = []
     let gain = ""
     const analysis = Object.values(drumsData)
+
+   
 
     const recursion = (arr, index) => {
       for (let i = 0; i < arr.length; i += 1) {
@@ -103,8 +106,13 @@ function SuperSeven() {
       gainSum.push(options.gain[element])
     })
 
-    if (gainSum.length === 0) {
+    if (gainSum.length === 0 && credits > 0) {
       setCredits(credits - totalBet)
+    }
+
+    if (credits === 0 || credits<0) {
+      setGameOver(true)
+      return
     }
 
     if (gainSum.length > 0) {
@@ -115,17 +123,13 @@ function SuperSeven() {
       if (increaseCredits.length > 0) {
         increaseCredits.reduce((acc, current) => {
           acc = acc + current
-          setGainData(acc)
-          console.log("acc", acc)
+          setGain(acc)
+          setCredits(credits + acc)
           return acc
         }, 0)
       }
-
-      setCredits(credits)
     }
   }
-
-  // console.log("gainData", gainData)
 
   const startGame = () => {
     setGameOn(!gameOn)
@@ -136,45 +140,102 @@ function SuperSeven() {
     })
 
     countGain()
+    return 1
+  }
+
+  const restartGame = () => {
+    setGameOver(false)
+    setGameOn(!gameOn)
+    setCredits(1000)
+    setGameBet(100)
+    setDrumsData({})
+  }
+
+
+  const autoStart = (func, count) => {
+func()
   }
 
   return (
-    <>
-      <div>
-        <label>
-          <button onClick={decreaseLines}>-</button>
-          <input readOnly value={lines}></input>
-          <button onClick={increaseLines}>+</button>
-        </label>
-        <label>
-          <button onClick={decreaseBet}>-</button> <input readOnly value={gameBet}></input>
-          <button onClick={increaseBet}>+</button>
-        </label>
-        <input readOnly value={credits}></input>
-        <input readOnly value={totalBet}></input>
-
-        <input readOnly value={gain}></input>
-      </div>
-
-      <div className={styles.SuperSeven}>
-        {Object.keys(drumsData).length === 0 ? (
-          <>
-            <Drum key={uuidv4()} data={["START", "START", "START"]}></Drum>
-            <Drum key={uuidv4()} data={["START", "START", "START"]}></Drum>
-            <Drum key={uuidv4()} data={["START", "START", "START"]}></Drum>
-          </>
+    <div className={styles.Parent}>
+      <div className={styles.Container}>
+        {gameOver ? (
+          <div className={styles.ButtonsReStartDiv}>
+            <p>GameOver! Out of credits.</p>
+            <button className={styles.RestartGameButton} onClick={restartGame}>
+              Restart Game
+            </button>
+          </div>
         ) : (
-          Object.values(drumsData).map((arr) => {
-            return <Drum key={uuidv4()} data={arr}></Drum>
-          })
+          <div className={styles.DrumsAndInputs}>
+            <div className={styles.SuperSeven}>
+              {Object.keys(drumsData).length === 0 ? (
+                <>
+                  <Drum key={uuidv4()} data={["LUCK", "LUCK", "LUCK"]}></Drum>
+                  <Drum key={uuidv4()} data={["LUCK", "LUCK", "LUCK"]}></Drum>
+                  <Drum key={uuidv4()} data={["LUCK", "LUCK", "LUCK"]}></Drum>
+                </>
+              ) : (
+                Object.values(drumsData).map((arr) => {
+                  return <Drum key={uuidv4()} data={arr}></Drum>
+                })
+              )}
+            </div>
+
+            <div className={styles.InputsDiv}>
+              <div className={styles.InputDiv}>
+                <span>LINES</span>
+                <div>
+                  <button className={styles.ButtonInInputs} onClick={decreaseLines}>
+                    -
+                  </button>
+                  <input readOnly value={lines}></input>
+                  <button className={`${styles.ButtonInInputs} ${styles.IncreaseButton}`} onClick={increaseLines}>
+                    +
+                  </button>
+                </div>
+              </div>
+              <div className={styles.InputDiv}>
+                <span>GAMEBET</span>
+                <div>
+                  <button className={styles.ButtonInInputs} onClick={decreaseBet}>
+                    -
+                  </button>
+                  <input readOnly value={gameBet}></input>
+                  <button className={`${styles.ButtonInInputs} ${styles.IncreaseButton}`} onClick={increaseBet}>
+                    +
+                  </button>
+                </div>
+              </div>
+              <div className={styles.InputDiv}>
+                <span>CREDITS</span>
+                <input readOnly value={credits}></input>
+              </div>
+              <div className={styles.InputDiv}>
+                <span>TOTALBET</span>
+                <input readOnly value={totalBet}></input>
+              </div>
+              <div className={styles.InputDiv}>
+                <span>GAIN</span>
+                <input readOnly value={gain}></input>
+              </div>
+            </div>
+
+            <div className={styles.ButtonsStartDiv}>
+              <button className={styles.ButtonStart} onClick={startGame}>
+                Start
+              </button>
+              <button className={styles.ButtonStart} onClick={() => autoStart(startGame, 10)}>
+                AutoStart
+              </button>
+            </div>
+          </div>
         )}
       </div>
-      <div>
-        <button onClick={startGame}>Start</button>
-        <button>AutoStart</button>
-      </div>
-    </>
+    </div>
   )
 }
 
 export default SuperSeven
+
+// credits / totalBet
